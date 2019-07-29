3c948e259af5c505ecb245e8fe33cfbd873fca65
李继隆 <delong1q2w3e@163.com>
2019-07-29 17:34:06 +0800

    创建模型对话框

--- src/GUI/MainWindow.py
+++ src/GUI/MainWindow.py
@@ -19,6 +19,42 @@ class ShowText(QTextEdit):
             self.append(str(item))
 
 
+class ModelWindow(QDialog):
+    def __init__(self, model):
+        super().__init__()
+        self.resize(600, 480)
+        self.setWindowTitle('矩阵模型窗口')
+
+        self.model = model
+        self.tree = ElementTree(self.model)
+        # self.window1 = ShowText()
+        self.window2 = ParameterTree()
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.tree, 1)
+        hbox.addWidget(self.window2, 3)
+
+        # layout.addWidget(window1)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addStretch(1)
+        button1 = QPushButton('计算')
+        button2 = QPushButton('关闭')
+        hbox2.addWidget(button1)
+        hbox2.addWidget(button2)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        vbox.addLayout(hbox2)
+
+        self.setLayout(vbox)
+
+        # self.tree.sendmsg.connect(self.window1.print_name)
+        self.tree.sendmsg.connect(self.window2.show_dict)
+        # button1.clicked.connect(self.modeling)
+
+
+
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -56,7 +92,10 @@ class MainWin(QWidget):
 
     def modeling(self):
         self.model = MainModel(self.line_group)
+        self.model_window = ModelWindow(self.model)
+
         print(len(self.model.equs))
+        self.model_window.exec()
 
 
 if __name__ == '__main__':
