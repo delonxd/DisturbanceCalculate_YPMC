e0cdfb8b28641573f293c8ca870c81f6155711e1
李继隆 <delong1q2w3e@163.com>
2019-09-27 17:30:08 +0800

    改变钢轨道床电阻计算 现在先计算一次参数再进行折算

--- src/Module/SubRail.py
+++ src/Module/SubRail.py
@@ -1,6 +1,7 @@
 from src.Module.PortNetwork import TwoPortNetwork
 from src.AbstractClass.Equation import *
 from src.Module.ParameterType import *
+import numpy as np
 
 
 # 钢轨段模型
@@ -34,8 +35,14 @@ class SubRailPi(TwoPortNetwork):
         z_trk = self.z_trk
         rd = self.rd
         length = self.track_length / 1000
-        y_tk = 1 / z_trk[freq].z / length
-        y_rd = 1 / rd * length / 2
+        z0 = z_trk[freq].z
+        y0 = 1 / rd
+        zc = np.sqrt(z0 / y0)
+        gama = np.sqrt(z0 * y0)
+        zii = zc * np.sinh(gama * length)
+        yii = (np.cosh(gama * length) - 1) / zc / np.sinh(gama * length)
+        y_tk = 1 / zii
+        y_rd = yii
         equ1 = Equation(name=self.name+'_方程1')
         equ2 = Equation(name=self.name+'_方程2')
         equ1.add_items(EquItem(self['I1'], -1),
@@ -53,3 +60,26 @@ class SubRailPi(TwoPortNetwork):
             equ2.add_items(EquItem(self['Im'], (m * y_tk)))
         return self.equs
 
+    # def get_equs(self, freq):
+    #     z_trk = self.z_trk
+    #     rd = self.rd
+    #     length = self.track_length / 1000
+    #     y_tk = 1 / z_trk[freq].z / length
+    #     y_rd = 1 / rd * length / 2
+    #     print(1/y_tk, 1/y_rd)
+    #     equ1 = Equation(name=self.name+'_方程1')
+    #     equ2 = Equation(name=self.name+'_方程2')
+    #     equ1.add_items(EquItem(self['I1'], -1),
+    #                    EquItem(self['U1'], (y_rd + y_tk)),
+    #                    EquItem(self['U2'], -y_tk))
+    #     equ2.add_items(EquItem(self['I2'], -1),
+    #                    EquItem(self['U1'], -y_tk),
+    #                    EquItem(self['U2'], (y_tk + y_rd)))
+    #     self.equs = EquationGroup(equ1, equ2)
+    #     if hasattr(self, 'mutual_trk'):
+    #         m_circuit = self.mutual_trk
+    #         m = length * 0.30
+    #         self.varb_dict['Im'] = m_circuit.varb_dict['I1']
+    #         equ1.add_items(EquItem(self['Im'], (m * (y_rd + y_tk))))
+    #         equ2.add_items(EquItem(self['Im'], (m * y_tk)))
+    #     return self.equs
