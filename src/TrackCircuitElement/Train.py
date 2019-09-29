1f4d02234f99bf9b3dcfb975926b7104d366485a
李继隆 <delong1q2w3e@163.com>
2019-09-29 15:12:27 +0800

    修正了列车绝对位置赋值的BUG

--- src/TrackCircuitElement/Train.py
+++ src/TrackCircuitElement/Train.py
@@ -4,12 +4,12 @@ from src.Module.OutsideElement import ROutside
 
 # 列车
 class Train(ElePack):
-    def __init__(self, name_base, posi_abs, parameter):
+    def __init__(self, name_base, posi, parameter):
         parent_ins = None
         super().__init__(parent_ins, name_base)
-        self.init_position(0)
+        self.init_position(posi)
         self.parameter = parameter
         self.element['分路电阻1'] = ROutside(parent_ins=self, name_base='分路电阻1',
                                          posi=0, z=self.parameter['Rsht_z'])
         self.set_ele_name(prefix='')
-        self.set_posi_abs(posi_abs)
+        self.set_posi_abs(0)
