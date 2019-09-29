f8aa8abe200dc572411753ad5d73c4b37a807767
李继隆 <delong1q2w3e@163.com>
2019-09-29 11:10:58 +0800

    添加了白俄2000A发送接收基类

--- src/Module/TcsrLib.py
+++ src/Module/TcsrLib.py
@@ -80,6 +80,45 @@ class ZPW2000A_ZN_PTSVA1(TCSR):
         self.config_varb()
 
 
+# ZPW2000A区间白俄配置
+class ZPW2000A_QJ_Belarus(TCSR):
+    def __init__(self, parent_ins, name_base,
+                 posi_flag, cable_length, mode, level):
+        super().__init__(parent_ins, name_base, posi_flag)
+        self.parameter = para = parent_ins.parameter
+        self.posi_flag = posi_flag
+        self.init_position(0)
+        self.flag_ele_list = True
+        self.flag_ele_unit = True
+        self.mode = mode
+        self.send_level = level
+
+        if self.mode == '发送':
+            self.add_child('1发送器', TcsrPower(self, '1发送器', para['z_pwr']))
+        elif self.mode == '接收':
+            self.add_child('1接收器', TcsrReceiver(self, '1接收器', para['Z_rcv']))
+        self.add_child('1_隔离盒', TcsrIsolationBelarus(self, '1_隔离盒',
+                                              para['Z_iso1_Belarus'],
+                                              para['Z_iso2_Belarus']))
+        self.add_child('2防雷', TcsrFL(self, '2防雷',
+                                     para['FL_z1_发送端'],
+                                     para['FL_z2_发送端'],
+                                     para['FL_n_发送端']))
+        self.add_child('3Cab', TPortCable(self, '3Cab', cable_length,
+                                          para['Cable_R'],
+                                          para['Cable_L'],
+                                          para['Cable_C']))
+        self.add_child('4TAD', TcsrTADBelarus(self, '4TAD',
+                                              para['TAD_z1_Belarus'],
+                                              para['TAD_z2_Belarus'],
+                                              para['TAD_n_Belarus']))
+        self.add_child('5BA', TcsrBA(self, '5BA', para['PT']))
+        self.add_child('6CA', TcsrCA(self, '6CA', para['CA_z_区间']))
+
+        self.md_list = self.get_md_list([])
+        self.config_varb()
+
+
 # ZPW2000A站内移频脉冲标准配置
 class ZPW2000A_YPMC_Normal(TCSR):
     def __init__(self, parent_ins, name_base,
