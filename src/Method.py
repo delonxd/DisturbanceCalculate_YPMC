e8ddb5a5e07d6ac40f947b2a4f70f4bc022cb31c
李继隆 <delong1q2w3e@163.com>
2019-10-24 15:46:16 +0800

    1、添加了自动获取电容数量的方法 2、添加了自动生成区段频率的方法

--- src/Method.py
+++ src/Method.py
@@ -1,5 +1,6 @@
 from src.AbstractClass.ElePack import *
-
+import numpy as np
+from src.FrequencyType import Freq
 
 def show_ele(vessel, para=''):
     if isinstance(vessel, (list, set)):
@@ -19,3 +20,52 @@ def show_ele(vessel, para=''):
                 print(key, ':', vessel[key])
             else:
                 print(vessel[key].__dict__[para])
+
+def generate_frqs(freq1, m_num):
+    frqs = list()
+    for _ in range(m_num):
+        frqs.append(freq1)
+        freq1 = freq1.copy()
+        freq1.change_freq()
+    return frqs
+
+def get_c_nums(m_frqs, m_lens):
+    c_nums = list()
+    for num in range(len(m_frqs)):
+        freq = m_frqs[num]
+        length = m_lens[num]
+        c_num = get_c_num(freq, length)
+        c_nums.append(c_num)
+    return c_nums
+
+def get_c_num(freq, length):
+    if 0 < length < 300:
+        index = 0
+    elif length == 300:
+        index = 1
+    elif length > 300:
+        index = int((length - 251) / 50)
+    else:
+        index  = 0
+
+    CcmpTable1 = [0, 5, 6, 7, 8, 9, 10, 10, 11, 12, 13, 14, 15, 15, 16, 17, 18, 19, 20, 20, 21, 22, 23, 24, 25]
+    CcmpTable2 = [0, 4, 5, 5, 6, 7,  7,  8,  8,  9, 10, 10, 11, 12, 12, 13, 13, 14, 15, 15, 16, 17, 17, 18, 18]
+
+    freq = freq.value
+    if freq == 1700 or freq == 2000:
+        table = CcmpTable1
+    elif freq == 2300 or freq == 2600:
+        table = CcmpTable2
+    else:
+        table = []
+
+    c_num = table[index]
+
+    return c_num
+
+if __name__ == '__main__':
+    m_lens = [700, 700, 700]
+    m_frqs = generate_frqs(Freq(2600), 3)
+    c_nums = get_c_nums(m_frqs, m_lens)
+
+    pass
