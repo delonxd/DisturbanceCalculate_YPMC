1528baaf8c69e5c7ae384baeb243c066439d79b6
Delon <delong1q2w3e@163.com>
2020-03-22 14:24:55 +0800

    LineGroup.py删除了增加跳线的方法

--- src/TrackCircuitElement/LineGroup.py
+++ src/TrackCircuitElement/LineGroup.py
@@ -1,5 +1,4 @@
 from src.AbstractClass.ElePack import *
-from src.TrackCircuitElement.JumperWires import *
 
 
 class LineGroup(ElePack):
@@ -15,14 +14,6 @@ class LineGroup(ElePack):
         self.get_ele_set(ele_set=set())
         self.set_ele_name(prefix='')
 
-    def add_jumpers(self, line1, line2, name_base, posi_abs):
-        jumper = JumperWireConnection(name_base, line1, line2, posi_abs)
-        self.add_child(name_base, jumper)
-        self.refresh()
-        pass
 
     def add_line(self, line):
         self.add_child(line.name_base, line)
-
-    def add_turnout(self, turnout):
-        self.add_child(turnout.name_base, turnout)
