630590447ab49c4a3ba1117cb78dddcdb2f03cc2
李继隆 <delong1q2w3e@163.com>
2020-04-23 16:43:25 +0800

    添加了用于讲数据存储并输出到各个sheet中的类和方法

--- src/Data2Excel.py
+++ src/Data2Excel.py
@@ -37,3 +37,79 @@ class Data2Excel:
 
             header_t = header[:columns]
             df_output.to_excel(writer1, sheet_name=key_name, index=False, header=header_t)
+
+
+class SheetDataGroup:
+    def __init__(self, sheet_names):
+        self.sheet_names = sheet_names
+        self.data_dict = {}
+        self.dataframes = {}
+        for name in sheet_names:
+            self.data_dict[name] = SheetData(name=name)
+
+
+    def add_new_row(self):
+        for ele in self.data_dict.values():
+            ele.add_new_row()
+
+    def add_new_sheet(self, sheet_name):
+        self.sheet_names.append(sheet_name)
+        self.data_dict[sheet_name] = SheetData(name=sheet_name)
+
+    def add_dataframes(self, sheet_name, dataframe):
+        self.dataframes[sheet_name] = dataframe
+
+    def add_data(self, sheet_name, data1):
+        if not sheet_name in self.data_dict.keys():
+            self.add_new_sheet(sheet_name=sheet_name)
+            self.data_dict[sheet_name].add_new_row()
+        self.data_dict[sheet_name].add_data(data1)
+
+    def write2excel(self, sheet_names, writer):
+        for name in sheet_names:
+            self.data_dict[name].write2excel(writer)
+
+    def config_header(self):
+        for ele in self.data_dict.values():
+            ele.config_header(header=None)
+
+    def __getitem__(self, key):
+        return self.data_dict[key]
+
+    def __setitem__(self, key, value):
+        self.data_dict[key] = value
+
+
+class SheetData:
+    def __init__(self, name):
+        self.name = name
+        self.data_list = []
+        self.header = []
+
+    def add_new_row(self):
+        self.data_list.append([])
+
+    def add_data(self, data):
+        self.data_list[-1].append(data)
+
+    def config_header(self, header):
+        self.header = []
+        len_colunm = 0
+        if header is None:
+            for data in self.data_list:
+                len_colunm = max(len_colunm, len(data))
+            self.header = list(range(len_colunm))
+        else:
+            self.header = header
+
+    def write2excel(self, writer):
+        df_output = pd.DataFrame(self.data_list)
+        df_output.to_excel(writer, sheet_name=self.name, index=False, header=self.header)
+        # df_output.to_excel(writer, sheet_name=self.name, index=False, header=None)
+
+    def __getitem__(self, key):
+        return self.data_list[key]
+
+    def __setitem__(self, key, value):
+        self.data_list[key] = value
+
